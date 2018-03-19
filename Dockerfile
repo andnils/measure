761c233d956e0be0c56e56de033b2a2fdcc972ce
Anders Nilsson <anders.nilsson@sigma.se>
2018-03-19 23:17:10 +0100

    Serve static files from jetty

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,7 @@
 FROM openjdk:8-jdk-alpine
-# TODO: copy html/js files
-# TODO: add postgres10
-# TODO: add nginx
-# TODO: set environment vars (MEASURE_HTTP_PORT, MEASURE_DB_URL, MEASURE_DB_USER, MEASURE_DB_PASSWORD)
+
 ADD target/uberjar/measure-0.1.0-SNAPSHOT-standalone.jar /srv/app.jar
+ADD dist/* /dist/
 
 EXPOSE 3000
 
--- src/main/clj/measure/routes.clj
+++ src/main/clj/measure/routes.clj
@@ -1,16 +1,11 @@
 (ns measure.routes
   (:require [compojure.core :refer [GET PUT POST DELETE routes context]]
             [ring.util.response :refer [response content-type charset]]
-            [ring.middleware.json :as middleware]
+            [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
+            [ring.middleware.file :refer [wrap-file]]
             [measure.db :as q]))
 
 
-
-(defn- text-response [data]
-  (-> data
-      (response)
-      (content-type "text/plain")
-      (charset "UTF-8")))
 
 (defn- json-response [data]
   (-> data
@@ -19,19 +14,21 @@
       (charset "UTF-8")))
 
 (defn find-hero-by-id [db id]
-  (json-response (q/find-hero-by-id db id)))
+  (json-response
+    (first (q/find-hero-by-id db id))))
 
 (defn find-all-heroes [db]
-  (json-response (q/find-all-heroes db)))
+  (json-response
+    (q/find-all-heroes db)))
 
 (defn insert-hero [db hero]
-  (json-response (q/insert-hero db hero)))
+  (json-response
+    (first (q/insert-hero db hero))))
 
 
 ;; Define all routes here
 (defn app-routes [db]
   (routes
-    (GET "/" [] (text-response "I'm a little tëapöt"))
     (context "/api" []
       (GET "/heroes/:id" [id] (find-hero-by-id db id))
       (GET "/heroes" [] (find-all-heroes db))
@@ -41,5 +38,6 @@
 ;; The handler function wraps the routes in middleware
 (defn handler [db]
   (-> (app-routes db)
-      (middleware/wrap-json-body {:keywords? true :bigdecimals? true})
-      (middleware/wrap-json-response)))
+      (wrap-json-body {:keywords? true :bigdecimals? true})
+      (wrap-json-response)
+      (wrap-file "./dist")))
